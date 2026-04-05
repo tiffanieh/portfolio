3801b6695691f2b0579256d5b8398367cf3f12e9
tiffanie <tiffaniehuang25@gmail.com>
2026-04-04 21:36:33 -0700

    astro config edits for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+
   vite: {
     plugins: [tailwindcss()],
   },
+  site: 'https://tiffanieh.github.io',
+  base: 'portfolio',
 });
